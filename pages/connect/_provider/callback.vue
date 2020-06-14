0e2f08844a924d0c52474eb14ee636a1bebf3055
Aleksej <git@aleksejdix.com>
2020-06-14 10:07:50 +0200

    add callback page

---
+++ pages/connect/_provider/callback.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  fetch() {
+    try {
+      const token = this.$auth.getToken(this.$auth.strategy.name)
+      console.log(token)
+
+      // const response = await this.$axios.get(
+      //   `/connect/${this.$route.params.provider}`
+      // )
+      // console.log(response)
+    } catch (error) {
+      console.log(error)
+      return error
+    }
+  }
+}
+</script>
+
+<style></style>
